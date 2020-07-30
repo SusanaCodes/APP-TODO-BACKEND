275c9ab44b75496ff8bb597df5fd7f691958a53c
Susana <susanamncwabe@outlook.com>
2020-07-30 16:53:40 +0100

    Status 403, error

--- handler.js
+++ handler.js
@@ -1,12 +1,19 @@
 const serverlessHttp = require("serverless-http");
 const express = require("express");
+// const { response } = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-app.get("/tasks", function () {});
+app.get("/tasks", function (request, response) {
+  response.status(200).send("Hello, this is your API!");
+});
 
 app.delete("/tasks", function () {});
 
 app.post("/tasks", function () {});
 
 app.put("/tasks", function () {});
+
+module.exports.app = serverlessHttp(app);
